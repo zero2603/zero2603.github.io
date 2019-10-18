b2be2e6f16ec1c1153d60228c8985c43f0455a90
zero2603 <20153713@student.hust.edu.vn>
2019-10-19 00:17:20 +0700

    gopanda

--- gopanda-docs/service-worker.js
+++ gopanda-docs/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.1dbfde71a92ce2b6887602f0085ddbda.js"
+  "/gopanda-docs/precache-manifest.77a0ed39bdab2581a7ec2e4dea53a1c8.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/gopanda-docs/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
