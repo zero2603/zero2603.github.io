30f5110ee0aa288f18469ae540f0ce0d9068382b
zero2603 <20153713@student.hust.edu.vn>
2019-10-18 23:57:26 +0700

    gopanda

---
+++ gopanda-docs/precache-manifest.52d1adcc828928798237156c1d999f7e.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ffc11bc450dcd5e895312110e8c89055",
+    "url": "/index.html"
+  },
+  {
+    "revision": "57de524899d1bc0f4e91",
+    "url": "/static/css/2.134fdbc6.chunk.css"
+  },
+  {
+    "revision": "a31963a5df0e00e6909f",
+    "url": "/static/css/main.2ba990cc.chunk.css"
+  },
+  {
+    "revision": "57de524899d1bc0f4e91",
+    "url": "/static/js/2.db818ade.chunk.js"
+  },
+  {
+    "revision": "a31963a5df0e00e6909f",
+    "url": "/static/js/main.e7380ed0.chunk.js"
+  },
+  {
+    "revision": "0a48da72be0446c0350e",
+    "url": "/static/js/runtime-main.7cb12135.js"
+  },
+  {
+    "revision": "d961fdfabbe512d2675de09af09f598b",
+    "url": "/static/media/jsoneditor-icons.d961fdfa.svg"
+  },
+  {
+    "revision": "0b4ac1dc75df35e169b70d7719afe4cc",
+    "url": "/static/media/notification.0b4ac1dc.ttf"
+  },
+  {
+    "revision": "5bee74caefdf9d0a834915f6c8eeb259",
+    "url": "/static/media/notification.5bee74ca.svg"
+  },
+  {
+    "revision": "651771e1df95c807c99608188d0a4287",
+    "url": "/static/media/notification.651771e1.woff"
+  },
+  {
+    "revision": "c0d3c94cd6112550c51d7d1ed13b9da1",
+    "url": "/static/media/notification.c0d3c94c.eot"
+  }
+]);
